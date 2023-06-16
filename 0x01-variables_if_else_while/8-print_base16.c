d2a873324a2051ded2434c8a927c98c6b618e73e
mohammedHassanAbdullah <moh5418067@gmail.com>
2023-06-16 12:00:24 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * HexaDIcimal
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; a++)
+		putchar(x);
+	for (a = 'a'; a <= 'f'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
