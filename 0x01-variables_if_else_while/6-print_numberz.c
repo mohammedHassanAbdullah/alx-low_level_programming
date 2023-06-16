2864224525be6bc836c8bb5204d2a7f565254245
mohammedHassanAbdullah <moh5418067@gmail.com>
2023-06-16 11:29:13 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Alpha bit
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 48; alpha < 58; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
