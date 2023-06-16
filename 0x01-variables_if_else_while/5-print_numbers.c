73a8affffaeeac99ae59fc59e51e163295c7e4af
mohammedHassanAbdullah <moh5418067@gmail.com>
2023-06-16 11:08:38 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Alpha bit
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alpha;
+
+	for (alpha = 0; alpha < 10; alpha++)
+	{
+		printf("%d", alpha);
+	}
+	printf('\n');
+	return (0);
+}
