456d357d3ec1f2d4787a10266977c8cb767d0427
mohammedHassanAbdullah <moh5418067@gmail.com>
2023-06-20 11:23:45 -0700

    new 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * 0-putchar.c
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+a new 0x02-functions_nested_loops
