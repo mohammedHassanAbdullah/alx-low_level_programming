ebaf374acdc914b20c032acc100f40b6c9337347
mohammedHassanAbdullah <moh5418067@gmail.com>
2023-06-20 13:15:28 -0700

    Mohamed Hassan

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+}
