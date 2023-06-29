da0d9c2b9dd18717242aea5699c214da8243b715
mohammedHassanAbdullah <moh5418067@gmail.com>
2023-06-29 12:41:38 -0700

    Mohamed Hassan

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,5 +11,10 @@ char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 
 #endif /*MAIN_H*/
